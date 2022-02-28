17fdb9566f83220867a34a6d5cf262a2da4d9a99
shalabhraj1990 <shalabhrajetc@gmail.com>
2022-02-28 07:32:48 +0530

    jdbc custtomised security

--- src/main/java/com/spring/security/config/JdbcSecurityConfig.java
+++ src/main/java/com/spring/security/config/JdbcSecurityConfig.java
@@ -1,5 +1,8 @@
 package com.spring.security.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,24 +11,29 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
-//@Component
-public class BasicInMemoryFormRoleBasedSecurityConfig extends WebSecurityConfigurerAdapter {
+@Component
+public class JdbcSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	DataSource dataSource;
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		PasswordEncoder encoder = passwordEncoder();
-		auth.inMemoryAuthentication().withUser("employee").password(encoder.encode("emp123")).roles("EMPLOYEE").and()
-				.withUser("user").password(encoder.encode("user123")).roles("USER").and().withUser("admin")
-				.password(encoder.encode("admin123")).roles("ADMIN");
+		auth.jdbcAuthentication().dataSource(dataSource);
+		// .withDefaultSchema()
+//				.withUser("employee").password(encoder.encode("emp123")).roles("EMPLOYEE").and().withUser("user")
+//				.password(encoder.encode("user123")).roles("USER").and().withUser("admin")
+//				.password(encoder.encode("admin123")).roles("ADMIN");
+		// for customized table
+//		.usersByUsernameQuery("SELECT * FROM world.users  where username = ?")
+//		.authoritiesByUsernameQuery("SELECT * FROM world.authorities where username = ?")
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/index").permitAll().antMatchers("/user").authenticated()
-				.antMatchers("/employee").hasAnyRole("EMPLOYEE", "ADMIN").antMatchers("/admin").hasRole("ADMIN")
-				.and()
-				.logout().logoutSuccessUrl("/index")
-				.and()
-				.formLogin();
+				.antMatchers("/employee").hasAnyRole("EMPLOYEE", "ADMIN").antMatchers("/admin").hasRole("ADMIN").and()
+				.logout().logoutSuccessUrl("/index").and().formLogin();
 
 	}
 
